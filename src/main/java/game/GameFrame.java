ece0900e9104aa478a452356626602f6f49e4a22
Rhuan Hianc <rhuanhianc123@gmail.com>
2022-03-09 22:24:16 -0300

    Update GameFrame.java

--- src/main/java/game/GameFrame.java
+++ src/main/java/game/GameFrame.java
@@ -1,5 +1,6 @@
 package game;
 
+import game.enemy.Enemy;
 import game.player.Player;
 
 import javax.swing.*;
@@ -15,11 +16,15 @@ public class GameFrame extends Canvas implements Runnable, KeyListener {
     public static  int HEIGHT = 160;
     private static  int SCALE = 3;
     private BufferedImage layer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-    Player player;
+    private Player player;
+    private Enemy enemy;
+    private Ball ball;
     public GameFrame() {
     this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
         this.addKeyListener(this);
         player = new Player( 100 , HEIGHT -10);
+        enemy = new Enemy( 100,  0);
+        ball = new Ball(100 , HEIGHT / 2 -1);
         JFrame frame = new JFrame("Pong");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
@@ -32,7 +37,8 @@ public class GameFrame extends Canvas implements Runnable, KeyListener {
     }
     public void tick() {
         player.tick();
-
+        enemy.tick();
+        ball.tick();
     }
     public void render() {
         BufferStrategy bs = this.getBufferStrategy();
@@ -44,6 +50,8 @@ public class GameFrame extends Canvas implements Runnable, KeyListener {
             g.setColor(Color.BLACK);
             g.fillRect(0, 0, WIDTH, HEIGHT);
             player.render(g);
+            enemy.render(g);
+            ball.render(g);
             g = bs.getDrawGraphics();
             g.drawImage(layer,0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
 
