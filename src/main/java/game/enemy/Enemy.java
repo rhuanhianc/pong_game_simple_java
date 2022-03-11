83d743b75e55630ee52aa4fbbd5f8d5874f08913
Rhuan Hianc <rhuanhianc123@gmail.com>
2022-03-10 23:59:33 -0300

    Update Enemy.java

--- src/main/java/game/enemy/Enemy.java
+++ src/main/java/game/enemy/Enemy.java
@@ -1,17 +1,20 @@
 package game.enemy;
 
+import game.GameFrame;
+
 import java.awt.*;
 
 public class Enemy {
     public double x, y;
-    private static final int WIDTH = 40;
-    private static final int HEIGHT = 10;
+    public static final int WIDTH = 40;
+    public static final int HEIGHT = 5;
 
     public Enemy(int x, int y) {
         this.x = x;
         this.y = y;
     }
     public void tick() {
+        x+= ( GameFrame.ball.x - x) * 0.2;
 
     }
 
