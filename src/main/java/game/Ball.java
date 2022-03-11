73bc9a56d383e71bde63321690e4054dd7af2b63
Rhuan Hianc <rhuanhianc123@gmail.com>
2022-03-10 23:59:38 -0300

    Update Ball.java

--- src/main/java/game/Ball.java
+++ src/main/java/game/Ball.java
@@ -9,15 +9,37 @@ public class Ball {
     private static final int WIDTH = 3;
     private static final int HEIGHT = 3;
     private double dx,dy;
-    public double speed =1.1;
+    public double speed =1.7;
     public Ball(int x, int y) {
         this.x = x;
         this.y = y;
-        this.dx = new Random().nextGaussian();
-        this.dy = new Random().nextGaussian();
+        int random = new Random().nextInt(90);
+        this.dx = Math.cos(Math.toRadians(random));
+        this.dy = Math.sin(Math.toRadians(random));
     }
 
     public void tick() {
+        if(x + (dx*speed) + WIDTH >= GameFrame.WIDTH){
+            dx *= -1;
+        }else if(x + (dx*speed) <= 0){
+            dx *= -1;
+        }
+        if(y >= GameFrame.HEIGHT){
+
+            return;
+        }else if (y <= 0){
+            new GameFrame();
+
+        }
+        Rectangle r = new Rectangle((int)(x+(dx*speed)), (int)(y+(dy*speed)), WIDTH, HEIGHT);
+        Rectangle rPlayer = new Rectangle(GameFrame.player.x, GameFrame.player.y, GameFrame.player.WIDTH, GameFrame.player.HEIGHT);
+        Rectangle rEnemy = new Rectangle((int) GameFrame.enemy.x, (int) GameFrame.enemy.y, GameFrame.enemy.WIDTH, GameFrame.enemy.HEIGHT);
+        if(r.intersects(rPlayer)){
+            dy *= -1;
+        }
+        if(r.intersects(rEnemy)){
+            dy *= -1;
+        }
         x+=dx * speed;
         y+=dy * speed;
     }
